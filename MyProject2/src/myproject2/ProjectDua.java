ef64f320f6e89828e52e76ff1ec6048673c75fb1
kuncara15 <89762832+kuncara15@users.noreply.github.com>
2022-08-29 20:58:13 +0700

    Add files via upload

---
+++ MyProject2/src/myproject2/ProjectDua.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject2;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ProjectDua {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("FFFFFFFF    IIIII     TTTTTTTTTTT");
+        System.out.println("F             I            T");
+        System.out.println("F             I            T");
+        System.out.println("FFFFFFFF      I            T");
+        System.out.println("F             I            T");
+        System.out.println("F             I            T");
+        System.out.println("F             I            T");
+        System.out.println("F           IIIII          T");
+        // TODO code application logic here
+    }
+    
+}
